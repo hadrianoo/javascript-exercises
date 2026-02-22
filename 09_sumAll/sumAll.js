dcb4a8a4804d7c9db361c20e422e0c79cf172305
Hadriano <57418210+hadrianoo@users.noreply.github.com>
2026-02-22 18:00:16 +0100

    Solve 09

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
+const sumAll = function (a, b) {
+    let result = 0;
+    if (a > b) {
+        [a, b] = [b, a]
+    }
+    if (a < 0 ||
+        b < 0 ||
+        !Number.isInteger(a) ||
+        !Number.isInteger(b))
+        return "ERROR";
 
+    for (let i = a; i <= b; i++) {
+        result += i;
+    }
+    return result;
 };
 
 // Do not edit below this line
