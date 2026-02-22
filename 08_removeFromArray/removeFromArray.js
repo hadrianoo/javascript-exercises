3675e9b8047b18edb669b036d3cfbfedad29bb09
Hadriano <57418210+hadrianoo@users.noreply.github.com>
2026-02-22 17:42:29 +0100

    Solve 08

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr, ...theArgs) {
+
+    arr = Array.from(new Set(arr));
+
+    for (let num of theArgs) {
+        if (arr.includes(num)) {
+            arr.splice(arr.findIndex((value) => value == num), 1)
+        };
+    };
+    return arr;
 };
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
