1b60430fb9c3b6f12fb6455f7e2b74a844e85c11
hadriano <57418210+hadrianoo@users.noreply.github.com>
2026-03-04 15:07:44 +0100

    Solve 16

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
+function yearsLived(born, death) {
+    return death === undefined ?
+        (new Date()).getFullYear() - born :
+        death - born
+}
 
+const findTheOldest = function (arr) {
+
+    return arr.reduce((obj, item) => {
+        const first = yearsLived(obj.yearOfBirth, obj.yearOfDeath);
+        const sec = yearsLived(item.yearOfBirth, item.yearOfDeath);
+
+        return sec > first ? item : obj;
+    })
 };
 
 // Do not edit below this line
