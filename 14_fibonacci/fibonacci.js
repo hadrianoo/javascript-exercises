09daa0afd0801538160d5a3eda11368b48ca902d
hadriano <57418210+hadrianoo@users.noreply.github.com>
2026-03-04 13:46:23 +0100

    Solve 14

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function (num) {
 
+    if (num == 1) return 1;
+    if (num == 0) return 0;
+    if (num < 0) return "OOPS";
+
+    let a = 0;
+    let b = 1;
+
+    for (let i = 1; i < num; i++) {
+        let temp = b;
+        b = a + b;
+        a = temp;
+    }
+
+    return b;
 };
 
 // Do not edit below this line
