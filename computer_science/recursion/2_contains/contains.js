8b9a9601187a185f41052b73a79253ca9eb6b723
hadriano <57418210+hadrianoo@users.noreply.github.com>
2026-05-17 19:21:30 +0200

    Solve

--- computer_science/recursion/2_contains/contains.js
+++ computer_science/recursion/2_contains/contains.js
@@ -1,6 +1,12 @@
-const contains = function() {
-  
+const contains = function (object, searchValue) {
+  const values = Object.values(object);
+
+  if (values.includes(searchValue)) return true;
+  const objectsToSearch = values.filter(
+    (item) => typeof item === "object" && item !== null,
+  );
+  return objectsToSearch.some((item) => contains(item, searchValue));
 };
-  
+
 // Do not edit below this line
 module.exports = contains;
