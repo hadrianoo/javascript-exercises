f8b640daa2da38227d529fb8ce4270347c3a7e83
hadriano <57418210+hadrianoo@users.noreply.github.com>
2026-05-18 16:00:00 +0200

    Solve

--- computer_science/recursion/4_permutations/permutations.js
+++ computer_science/recursion/4_permutations/permutations.js
@@ -1,6 +1,16 @@
-const permutations = function() {
-  
+const permutations = function (inputArray) {
+  if (inputArray.length === 0) return [[]];
+  if (inputArray.length === 1) return [inputArray];
+
+  const answer = [];
+  for (let i = 0; i < inputArray.length; i++) {
+    const tempArray = permutations(
+      inputArray.filter((item, index) => index !== i),
+    ).map((item) => [inputArray[i], ...item]);
+
+    answer.push(...tempArray);
+  }
+  return answer;
 };
-  
 // Do not edit below this line
 module.exports = permutations;
