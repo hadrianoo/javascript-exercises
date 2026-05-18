80d78b3b6b432583c7e2e6a1a52fd082cbe91c1b
hadriano <57418210+hadrianoo@users.noreply.github.com>
2026-05-18 11:49:03 +0200

    Solve

--- computer_science/recursion/3_totalIntegers/totalIntegers.js
+++ computer_science/recursion/3_totalIntegers/totalIntegers.js
@@ -1,6 +1,17 @@
-const totalIntegers = function() {
-  
+function isObject(value) {
+  return typeof value === "object" && value !== null;
+}
+
+const totalIntegers = function (obj) {
+  if (!isObject(obj)) return;
+  const values = Object.values(obj);
+
+  return values.reduce((sum, item) => {
+    if (Number.isInteger(item)) return sum + 1;
+    if (isObject(item)) return sum + totalIntegers(item);
+    return sum;
+  }, 0);
 };
-  
+
 // Do not edit below this line
 module.exports = totalIntegers;
