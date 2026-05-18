213170fb91dc17997fb0587ca6bf3008bfc41af1
hadriano <57418210+hadrianoo@users.noreply.github.com>
2026-05-18 19:56:06 +0200

    Solve

--- computer_science/recursion/5_pascal/pascal.js
+++ computer_science/recursion/5_pascal/pascal.js
@@ -1,6 +1,14 @@
-const pascal = function() {
-  
+const pascal = function (n) {
+  if (n === 1) return [1];
+
+  let newArr = [];
+  const array = [0, ...pascal(n - 1), 0];
+
+  for (let i = 0; i < n; i++) {
+    newArr.push(array[i] + array[i + 1]);
+  }
+  return newArr;
 };
-  
+
 // Do not edit below this line
 module.exports = pascal;
